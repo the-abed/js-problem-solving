dd6163640431db4f20ed76e484c471ad19debfab
the-abed <mohammadabed1283@gmail.com>
2026-01-08 18:54:28 +0600

    solved problem 6

--- index.js
+++ index.js
@@ -71,3 +71,15 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // [1,2,3,4]
+
+//Problem 6: Sum of All Numbers in an Array
+function sumArray(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4])); // 10
