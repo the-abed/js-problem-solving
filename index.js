40248965c61730321c26a4fb4025fecfca9cad8a
the-abed <mohammadabed1283@gmail.com>
2026-01-08 18:52:43 +0600

    solved problem 4

--- index.js
+++ index.js
@@ -26,4 +26,33 @@ function countVowels(str) {
 
 console.log(countVowels("programming")); // 3
 
+// Problem 3: Check for Palindrome
 
+function isPalindrome(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+
+
+// Problem 4: Find the Maximum Number
+
+function findMax(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findMax([5, 1, 9, 3])); // 9
