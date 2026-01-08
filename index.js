4aa3d3940c990cd2997c8c5d8c31742b400314b5
the-abed <mohammadabed1283@gmail.com>
2026-01-08 18:53:52 +0600

    solved problem 5

--- index.js
+++ index.js
@@ -56,3 +56,18 @@ function findMax(arr) {
 }
 
 console.log(findMax([5, 1, 9, 3])); // 9
+
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // [1,2,3,4]
