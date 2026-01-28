b5b7ea1f10f71f6231d45b9b3eee4b47c6b73208
the-abed <mohammadabed1283@gmail.com>
2026-01-28 23:40:21 +0600

    add new

--- index.js
+++ index.js
@@ -212,3 +212,16 @@ students.sort((a, b) => b.marks - a.marks);
 console.log(students);
 
 
+// Problem 17: Find Duplicate Elements in an Array
+function findDuplicates(arr) {
+  let seen = new Set();
+  let duplicates = new Set();
+
+  for (let num of arr) {
+    if (seen.has(num)) duplicates.add(num);
+    else seen.add(num);
+  }
+  return [...duplicates];
+}
+
+console.log(findDuplicates([1, 2, 3, 2, 4, 1]));
